1ca663257cbf15f4a4a48e268123962b5f1d41b0
Brice Habets <brice.habets@gmail.com>
2020-04-10 16:52:45 +0200

    Logging in works with API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,4 +23,20 @@ class AuthController extends Controller
 
         return response(['user' => $user, 'access_token' => $accessToken]);
     }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required',
+        ]);
+
+        if (!auth()->attempt($loginData)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+    }
 }
