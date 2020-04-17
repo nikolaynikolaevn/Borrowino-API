61be0066eb2619a89f3d911cb880b53015edb992
Brice Habets <brice.habets@gmail.com>
2020-04-17 20:02:06 +0200

    Test pagination works

--- tests/Http/Controllers/OfferControllerTest.php
+++ tests/Http/Controllers/OfferControllerTest.php
@@ -22,12 +22,33 @@ class OfferControllerTest extends TestCase
         // Act
 
         // Assert
-        $this->get(route('offer.index'))
+        $this->getJson(route('offer.index'))
             ->assertStatus(200)
             ->assertJsonStructure([
                 'data' => ['*' => ['id', 'created_at', 'updated_at', 'title', 'description', 'location', 'price', 'owner']]
             ])
-            ->assertJson(['data' => $offersExpected->toArray()])
-        ;
+            ->assertJson(['data' => $offersExpected->toArray()]);
+    }
+
+    /**
+     * @test
+     */
+    public function index_paginationWorks()
+    {
+        // Arrange
+        $offersExpected = factory(Offer::class, 16)->create();
+        $queryParameters = 'page=2';
+
+        // Act
+        $response = $this->get('/api/offer' . '?' . $queryParameters); // this is a custom function. you can use $this->get(...)
+
+        // Assert
+        $this->assertEquals('200', $response->getStatusCode());
+
+        // convert JSON response string to Array
+        $responseArray = json_decode($response->getContent());
+
+        // assert the second page returned the "x" additional data
+        $this->assertCount(1, $responseArray->data);
     }
 }
