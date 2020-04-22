307a0275dc5e076994e56e7ae2a235a94bedb200
Brice Habets <brice.habets@gmail.com>
2020-04-22 13:49:25 +0200

    Allow anonymous users to view offers

--- routes/api.php
+++ routes/api.php
@@ -13,16 +13,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('/register', 'AuthController@register')->name('register');
 Route::post('/login', 'AuthController@login')->name('login');
 
-Route::middleware('auth:api')->group(function (){
-    Route::apiResource('offer', 'OfferController');
+Route::get('offer', 'OfferController@index')->name('offer.index');
+Route::get('offer/{offer}', 'OfferController@show')->name('offer.show');
+
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('offer', 'OfferController')->except(['index', 'show']);
 });
 
 
---
+++ tests/Feature/RouterTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Offer;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RouterTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * @test
+     */
+    public function anonymousUser_canSee_offerIndex()
+    {
+        // Arrange
+        factory(Offer::class, 2)->create();
+
+        // Act
+        $request = $this->getJson(route('offer.index'));
+
+        //Assert
+        $request->assertStatus(200);
+    }
+
+    use RefreshDatabase;
+    /**
+     * @test
+     */
+    public function anonymousUser_canSee_offerShow()
+    {
+        // Arrange
+        factory(Offer::class)->create();
+
+        // Act
+        $request = $this->getJson(route('offer.index',1));
+
+        // Assert
+        $request->assertStatus(200);
+    }
+
+}
