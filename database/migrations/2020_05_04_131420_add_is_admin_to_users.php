0d23008f4c101a4b8b4b48f3288cf4da1caf3ff4
Brice Habets <brice.habets@gmail.com>
2020-05-16 20:05:30 +0200

    Change field to conform to SQL standard

--- database/migrations/2020_05_04_131420_add_is_admin_to_users.php
+++ database/migrations/2020_05_04_131420_add_is_admin_to_users.php
@@ -14,7 +14,7 @@ class AddIsAdminToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('isAdmin')->nullable();
+            $table->boolean('is_admin')->default(false);
         });
     }
 
@@ -26,7 +26,7 @@ class AddIsAdminToUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['isAdmin']);
+            $table->dropColumn(['is_admin']);
         });
     }
 }
