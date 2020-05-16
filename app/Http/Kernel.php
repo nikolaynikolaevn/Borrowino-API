77dc508b9e4dcbc56f2b0fc283fe2dcbf4c18249
Brice Habets <brice.habets@gmail.com>
2020-05-16 20:51:43 +0200

    Change admin middleware registering
    This makes it easier to refactor if needed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin'=>'App\Http\Middleware\checkAdmin'
+        'admin' => \App\Http\Middleware\CheckAdmin::class,
     ];
 }
