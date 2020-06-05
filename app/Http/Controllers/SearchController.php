5d246cb2f806bb71e7610a16911f26cb611c20c2
Nikolay Nikolaev <nikolaynikolaevvnn@gmail.com>
2020-06-05 18:06:54 +0200

    Add user search by name with paginated results

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Offer;
+use App\User;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
     public function searchOffer(Request $request) {
         $query = $request->input('q');
-        $offers = Offer::where('title', 'LIKE', "%{$query}%")->orWhere('description', 'LIKE', "%{$query}%")->get();
-        return response()->json($offers, 200);
+        $type = $request->input('type');
+
+        if ($type === "users") {
+            $result = User::where('name', 'LIKE', "%{$query}%")->paginate(15);
+        }
+        else {
+            $result = Offer::where('title', 'LIKE', "%{$query}%")->orWhere('description', 'LIKE', "%{$query}%")->paginate(15);
+        }
+        return response()->json($result, 200);
     }
 }
